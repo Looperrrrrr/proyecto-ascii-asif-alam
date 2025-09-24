6944d8b9257a553f27a6ada5262f90671644bf5a
Asif Alam <104073769+Looperrrrrr@users.noreply.github.com>
2025-09-24 14:10:55 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+const asciiHeart = [
+    "⣠⣤⣶⣶⣦⣄⡀  ⠀⢀⣤⣴⣶⣶⣤⣀",
+    "⣼⣿⣿⣿⣿⣿⣿⣷⣤⣾⣿⣿⣿⣿⣿⣿⣧",
+    "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+    "⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏",
+    " ⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋ ",
+    "   ⠙⢿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀",
+    "     ⠉⢿⣿⣿⣿⠟⠋⠀",
+    "⠀      ⠙⠻⠁"
+]
+
+const printByLine = (ms) => {
+    for (let index = 0; index < asciiHeart.length; index++) {
+        setTimeout(() => {
+            console.log(asciiHeart[index])
+        }, ms*index);
+
+    }
+
+}
+
+console.log(printByLine(200));
+
+
+
+
+
